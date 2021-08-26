9c1956c354fc50729fa6f706082314d47b6c4f34
kandari012 <73176498+kandari012@users.noreply.github.com>
2021-08-26 15:48:39 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">KANDARI</div>;
+  return (
+    <div className="App">
+      KANDARI
+      <p>how are you</p>
+    </div>
+  );
 }
 
 export default App;
